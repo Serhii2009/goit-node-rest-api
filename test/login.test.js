013e6193bbda197516813283f241c5264f9070c8
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-04-30 00:26:48 +0300

    Update login.test.js

--- test/login.test.js
+++ test/login.test.js
@@ -1,41 +1,41 @@
-import mongoose from "mongoose";
-import request from "supertest";
+import mongoose from 'mongoose'
+import request from 'supertest'
 
-import app from "../app.js";
+import app from '../app.js'
 
-describe("Test Login Controller", () => {
+describe('Test Login Controller', () => {
   beforeAll(async () => {
-    await mongoose.connect(process.env.DB_HOST);
-  });
+    await mongoose.connect(process.env.DB_HOST)
+  })
 
   afterAll(async () => {
-    await mongoose.connection.close();
-  });
+    await mongoose.connection.close()
+  })
 
-  test("should return status code 200", async () => {
-    const response = await request(app).post("/users/login").send({
-      email: "elenaa@mail.com",
-      password: "123456",
-    });
-    expect(response.status).toBe(200);
-  });
+  test('should return status code 200', async () => {
+    const response = await request(app).post('/users/login').send({
+      email: 'elenaa@mail.com',
+      password: '123456',
+    })
+    expect(response.status).toBe(200)
+  })
 
-  test("should return a token in the response", async () => {
-    const response = await request(app).post("/users/login").send({
-      email: "elenaa@mail.com",
-      password: "123456",
-    });
-    expect(response.body.token).toBeDefined();
-  });
+  test('should return a token in the response', async () => {
+    const response = await request(app).post('/users/login').send({
+      email: 'elenaa@mail.com',
+      password: '123456',
+    })
+    expect(response.body.token).toBeDefined()
+  })
 
-  test("should return a user object with email and subscription fields as strings", async () => {
-    const response = await request(app).post("/users/login").send({
-      email: "elenaa@mail.com",
-      password: "123456",
-    });
-    expect(response.body.user).toHaveProperty("email");
-    expect(response.body.user).toHaveProperty("subscription");
-    expect(typeof response.body.user.email).toBe("string");
-    expect(typeof response.body.user.subscription).toBe("string");
-  });
-});
+  test('should return a user object with email and subscription fields as strings', async () => {
+    const response = await request(app).post('/users/login').send({
+      email: 'elenaa@mail.com',
+      password: '123456',
+    })
+    expect(response.body.user).toHaveProperty('email')
+    expect(response.body.user).toHaveProperty('subscription')
+    expect(typeof response.body.user.email).toBe('string')
+    expect(typeof response.body.user.subscription).toBe('string')
+  })
+})
