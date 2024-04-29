1867fa09477b964fb2ad0915e0a630ca703472ad
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-04-29 12:59:47 +0300

    add

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
-import contactsRouter from "./routes/contactsRouter.js";
-import authRouter from "./routes/authRouter.js";
+import contactsRouter from './routes/contactsRouter.js'
+import authRouter from './routes/authRouter.js'
 
-const app = express();
-dotenv.config();
+const app = express()
+dotenv.config()
 
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
 
-app.use("/users", authRouter);
-app.use("/api/contacts", contactsRouter);
+app.use('/users', authRouter)
+app.use('/api/contacts', contactsRouter)
 
 app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
+  res.status(404).json({ message: 'Route not found' })
+})
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
+  const { status = 500, message = 'Server error' } = err
+  res.status(status).json({ message })
+})
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    console.log("Database connection successful");
+    console.log('Database connection successful')
     app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`);
-    });
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
   })
   .catch((err) => {
-    console.log(err.message);
-    process.exit(1);
-  });
+    console.log(err.message)
+    process.exit(1)
+  })
 
-export default app;
+export default app
